9d60960a72e39230ea490d2d8695b639137ebe9f
austin_m <1austinm@protonmail.com>
2022-04-26 18:09:06 -0500

    Shadowing

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,21 @@ fn main() {
     // Constant variables must be type annotated unlike regular variables
     // 
     const TWENTY_FOUR_HOURS_IN_MINUTES: u32 = 24 * 60;
-    
+
+    // Shadowing? Can't think of what I would need this for yet but...
+    // Shadowing vs Mutable: With shadowing the type can be changed, with a regular mutable variable it cannot
+    //Since we are using the 'let' keyword to redeclare the variable, it's not an error even though the variable is immutable
+    let shadowed = 5;
+    let shadowed = shadowed + 1;
+
+    {
+        let shadowed = shadowed * 2;
+        println!("The value of the shadowed variable in the inner scope is: {}", shadowed);
+    }
+
+    println!("The value of the shadowed variable is: {}", shadowed);
+
+    // Example of shadowing's use
+    let spaces = "   ";
+    let spaces = spaces.len();
 }
