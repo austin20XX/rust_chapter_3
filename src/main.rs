ffd2e2a1c1876a662bc521bf3e1db170bb6a0422
austin_m <1austinm@protonmail.com>
2022-04-26 17:25:47 -0500

    Create mutable variable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let x = 5;
     println!("The value of x is: {}", x);
-    x = 6;
+    // This line would produce an error, because by default variables are immutable in rust
+    // x = 6;
     println!("The new value of x is: {}", x);
+
+    let mut y = 10;
+    println!("The value of y is: {}", y);
+    y = 20; 
+    println!("The value of y is: {}", y);
 }
